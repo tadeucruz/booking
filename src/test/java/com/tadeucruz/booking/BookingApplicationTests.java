2ae52317ef574a4de16dec6b249f47e483113a43
Tadeu Cruz <tadeucruz@gmail.com>
2022-11-13 11:59:33 -0300

    Initial Commit

---
+++ src/test/java/com/tadeucruz/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadeucruz.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
