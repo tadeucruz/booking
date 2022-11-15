9fd1860ac16eef7c5e02998682492208a499ec43
Tadeu Cruz <tadeucruz@gmail.com>
2022-11-15 17:49:01 -0300

    Initial support for swagger 3

--- src/main/java/com/tadeucruz/booking/model/rest/CreateBookingRequest.java
+++ src/main/java/com/tadeucruz/booking/model/rest/CreateBookingRequest.java
@@ -1,6 +1,9 @@
 package com.tadeucruz.booking.model.rest;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,9 +11,22 @@ import lombok.Data;
 @Builder
 public class CreateBookingRequest {
 
+    @Schema(type = "string", example = "1")
+    @NotBlank
     private Integer roomId;
+
+    @Schema(type = "string", example = "44")
+    @NotBlank
     private Integer userId;
+
+    @Schema(type = "date", example = "2022-11-15")
+    @NotBlank
+    @Future
     private LocalDate startDate;
+
+    @Schema(type = "date", example = "2022-11-17")
+    @NotBlank
+    @Future
     private LocalDate endDate;
 
 }
